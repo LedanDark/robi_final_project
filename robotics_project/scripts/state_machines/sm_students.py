5aa933d7dfa23dd34646a6f366c470f78476e356
LedanDark <fil@furhatrobotics.com>
2020-09-27 12:29:20 +0200

    Turning around and moving forward

--- robotics_project/scripts/state_machines/sm_students.py
+++ robotics_project/scripts/state_machines/sm_students.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import numpy as np
-from numpy import linalg as LA
+from numpy import linalg as LA, math
 
 import rospy
 from geometry_msgs.msg import Twist
-from std_srvs.srv import Empty, SetBool, SetBoolRequest  
+from std_srvs.srv import Empty, SetBool, SetBoolRequest
 from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from robotics_project.srv import MoveHead, MoveHeadRequest, MoveHeadResponse
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
@@ -16,15 +16,17 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from nav_msgs.msg import Odometry
 
 from moveit_msgs.msg import MoveItErrorCodes
+
 moveit_error_dict = {}
 for name in MoveItErrorCodes.__dict__.keys():
     if not name[:1] == '_':
         code = MoveItErrorCodes.__dict__[name]
         moveit_error_dict[code] = name
 
+
 class StateMachine(object):
     def __init__(self):
-        
+
         self.node_name = "Student SM"
 
         # Access rosparams
@@ -52,21 +54,20 @@ class StateMachine(object):
         rospy.sleep(3)
         self.check_states()
 
-
     def check_states(self):
 
         while not rospy.is_shutdown() and self.state != 4:
-            
-            # State 0: Move the robot "manually" to door
+
+            # Turn around
             if self.state == 0:
                 move_msg = Twist()
-                move_msg.linear.x = 1
+                move_msg.angular.z = -1
 
                 rate = rospy.Rate(10)
                 converged = False
                 cnt = 0
-                rospy.loginfo("%s: Moving towards door", self.node_name)
-                while not rospy.is_shutdown() and cnt < 25:
+                rospy.loginfo("%s: Turning around", self.node_name)
+                while not rospy.is_shutdown() and cnt < 30:
                     self.cmd_vel_pub.publish(move_msg)
                     rate.sleep()
                     cnt = cnt + 1
@@ -74,68 +75,65 @@ class StateMachine(object):
                 self.state = 1
                 rospy.sleep(1)
 
-            # State 1:  Tuck arm 
+            # Move towards the table
             if self.state == 1:
-                rospy.loginfo("%s: Tucking the arm...", self.node_name)
-                goal = PlayMotionGoal()
-                goal.motion_name = 'home'
-                goal.skip_planning = True
-                self.play_motion_ac.send_goal(goal)
-                success_tucking = self.play_motion_ac.wait_for_result(rospy.Duration(100.0))
-
-                if success_tucking:
-                    rospy.loginfo("%s: Arm tuck: ", self.play_motion_ac.get_result())
-                    self.state = 2
-                else:
-                    self.play_motion_ac.cancel_goal()
-                    rospy.logerr("%s: play_motion failed to tuck arm, reset simulation", self.node_name)
-                    self.state = 5
-
-                rospy.sleep(1)
-
-            # State 2:  Move the robot "manually" to chair
-            if self.state == 2:
                 move_msg = Twist()
-                move_msg.angular.z = -1
+                move_msg.linear.x = 1
 
                 rate = rospy.Rate(10)
                 converged = False
                 cnt = 0
                 rospy.loginfo("%s: Moving towards table", self.node_name)
-                while not rospy.is_shutdown() and cnt < 5:
+                while not rospy.is_shutdown() and cnt < 8:
                     self.cmd_vel_pub.publish(move_msg)
                     rate.sleep()
                     cnt = cnt + 1
 
-                move_msg.linear.x = 1
-                move_msg.angular.z = 0
-                cnt = 0
-                while not rospy.is_shutdown() and cnt < 15:
-                    self.cmd_vel_pub.publish(move_msg)
-                    rate.sleep()
-                    cnt = cnt + 1
-
-                self.state = 3
+                self.state = 2
                 rospy.sleep(1)
+            # # State 1:  Tuck arm
+            # if self.state == 1:
+            #     rospy.loginfo("%s: Tucking the arm...", self.node_name)
+            #     goal = PlayMotionGoal()
+            #     goal.motion_name = 'home'
+            #     goal.skip_planning = True
+            #     self.play_motion_ac.send_goal(goal)
+            #     success_tucking = self.play_motion_ac.wait_for_result(rospy.Duration(100.0))
+            #
+            #     if success_tucking:
+            #         rospy.loginfo("%s: Arm tuck: ", self.play_motion_ac.get_result())
+            #         self.state = 2
+            #     else:
+            #         self.play_motion_ac.cancel_goal()
+            #         rospy.logerr("%s: play_motion failed to tuck arm, reset simulation", self.node_name)
+            #         self.state = 5
+            #
+            #     rospy.sleep(1)
 
-            # State 3:  Lower robot head service
-            if self.state == 3:
-            	try:
-                    rospy.loginfo("%s: Lowering robot head", self.node_name)
-                    move_head_srv = rospy.ServiceProxy(self.mv_head_srv_nm, MoveHead)
-                    move_head_req = move_head_srv("down")
-                    
-                    if move_head_req.success == True:
-                        self.state = 4
-                        rospy.loginfo("%s: Move head down succeded!", self.node_name)
-                    else:
-                        rospy.loginfo("%s: Move head down failed!", self.node_name)
-                        self.state = 5
-
-                    rospy.sleep(3)
-                
-                except rospy.ServiceException, e:
-                    print "Service call to move_head server failed: %s"%e
+            # # State 2:  Move the robot "manually" to chair
+            # if self.state == 2:
+            #     move_msg = Twist()
+            #     move_msg.angular.z = -1
+            #
+            #     rate = rospy.Rate(10)
+            #     converged = False
+            #     cnt = 0
+            #     rospy.loginfo("%s: Moving towards table", self.node_name)
+            #     while not rospy.is_shutdown() and cnt < 5:
+            #         self.cmd_vel_pub.publish(move_msg)
+            #         rate.sleep()
+            #         cnt = cnt + 1
+            #
+            #     move_msg.linear.x = 1
+            #     move_msg.angular.z = 0
+            #     cnt = 0
+            #     while not rospy.is_shutdown() and cnt < 15:
+            #         self.cmd_vel_pub.publish(move_msg)
+            #         rate.sleep()
+            #         cnt = cnt + 1
+            #
+            #     self.state = 3
+            #     rospy.sleep(1)
 
             # Error handling
             if self.state == 5:
@@ -264,7 +262,7 @@ class StateMachine(object):
 # 		# already tucked the arm
 # 		if self.finished: 
 # 			return pt.common.Status.SUCCESS
-		
+
 # 		# command to tuck arm if haven't already
 # 		elif not self.sent_goal:
 
@@ -285,7 +283,6 @@ class StateMachine(object):
 # 		# if I'm still trying :|
 # 		else:
 # 			return pt.common.Status.RUNNING
-		
 
 
 # class LowerHead(pt.behaviour.Behaviour):
@@ -320,15 +317,13 @@ class StateMachine(object):
 # 		else: return pt.common.Status.SUCCESS if self.move_head_req.success else pt.common.Status.FAILURE
 
 
-	
-
 if __name__ == "__main__":
 
-	rospy.init_node('main_state_machine')
-	try:
-		#StateMachine()
-		StateMachine()
-	except rospy.ROSInterruptException:
-		pass
+    rospy.init_node('main_state_machine')
+    try:
+        # StateMachine()
+        StateMachine()
+    except rospy.ROSInterruptException:
+        pass
 
-	rospy.spin()
+    rospy.spin()
