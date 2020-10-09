866c4bccd806b41169965504e5cb8659e90f18bb
LedanDark <fil@furhatrobotics.com>
2020-10-09 10:51:37 +0200

    fixes file setup.

--- robotics_project/scripts/behaviour_trees/bt_students.py
+++ robotics_project/scripts/behaviour_trees/bt_students.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 import py_trees as pt, py_trees_ros as ptr, rospy
-from reactive_sequence import RSequence
-import numpy
-import py_trees as pt
-import rospy
 from actionlib import SimpleActionClient
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState, SetModelStateRequest
@@ -14,60 +10,9 @@ from robotics_project.srv import MoveHead
 from std_srvs.srv import SetBool, Empty
 from geometry_msgs.msg import PoseArray, Pose, Point, Vector3, Quaternion
 import math
-
-class BehaviourTree(ptr.trees.BehaviourTree):
-
-    def __init__(self):
-        rospy.loginfo("Initialising behaviour tree")
-        # Reset head up for gazebo simulation failures to reset robot configuration.
-        mv_head_srv_nm = rospy.get_param(rospy.get_name() + '/move_head_srv')
-        move_head_srv = rospy.ServiceProxy(mv_head_srv_nm, MoveHead)
-        blackboard = pt.py_trees.blackboard.Blackboard()
-        blackboard.mapIsDirty = True
-        blackboard.cubeLocation = "A"  # A, Hand, or B
-        blackboard.robotLocation = None  # None, A, or B
-        blackboard.resetTuckArm = False
-        blackboard.resetPick = False
-        blackboard.resetPlace = False
-
-        relocateCube = RSequence(name="Cube sequence",
-                                 children=[tuckarm(),
-                                           localization(),
-                                           navigateToGoal(),
-                                           retrieveCube(),
-                                           placeDownCube(),
-                                           resetmission()
-                                           ])
-        tree = pt.composites.Selector(name="Main sequence",
-                                      children=[missionChecker(), relocateCube]
-                                      )
-        super(BehaviourTree, self).__init__(tree)
-        # ensure head is up before starting, otherwise need to reset gazebo every time.
-        move_head_srv("up")
-        # execute the behaviour tree
-        rospy.sleep(5)
-        self.setup(timeout=10000)
-        while not rospy.is_shutdown():
-            self.tick_tock(1)
-
-
-def localization():
-    return pt.composites.Selector(
-        name="Checked localization",
-        children=[isMapClean(), localizeBehaviour()]
-    )
-
-
-if __name__ == "__main__":
-
-    rospy.init_node('main_state_machine')
-    try:
-        BehaviourTree()
-    except rospy.ROSInterruptException:
-        pass
-
-    rospy.spin()
-
+import numpy
+import rospy
+from reactive_sequence import RSequence
 
 class localizeBehaviour(pt.behaviour.Behaviour):
     def calculateConvergance(self, poseArray):
@@ -512,3 +457,57 @@ class missionChecker(pt.behaviour.Behaviour):
         # Limitation, will not see cube teleporting away. Could we have "can see cube"? Only get update when we seee it..
         # Could have a timer counter, that increases on this function call and decreases on succesful tick.
         self.blackboard.cubeLocation = self.blackboard.robotLocation
+
+
+class BehaviourTree(ptr.trees.BehaviourTree):
+
+    def __init__(self):
+        rospy.loginfo("Initialising behaviour tree")
+        # Reset head up for gazebo simulation failures to reset robot configuration.
+        mv_head_srv_nm = rospy.get_param(rospy.get_name() + '/move_head_srv')
+        move_head_srv = rospy.ServiceProxy(mv_head_srv_nm, MoveHead)
+        blackboard = pt.blackboard.Blackboard()
+        blackboard.mapIsDirty = True
+        blackboard.cubeLocation = "A"  # A, Hand, or B
+        blackboard.robotLocation = None  # None, A, or B
+        blackboard.resetTuckArm = False
+        blackboard.resetPick = False
+        blackboard.resetPlace = False
+
+        relocateCube = RSequence(name="Cube sequence",
+                                 children=[tuckarm(),
+                                           localization(),
+                                           navigateToGoal(),
+                                           retrieveCube(),
+                                           placeDownCube(),
+                                           resetmission()
+                                           ])
+        tree = pt.composites.Selector(name="Main sequence",
+                                      children=[missionChecker(), relocateCube]
+                                      )
+        super(BehaviourTree, self).__init__(tree)
+        # ensure head is up before starting, otherwise need to reset gazebo every time.
+        move_head_srv("up")
+        # execute the behaviour tree
+        rospy.sleep(5)
+        self.setup(timeout=10000)
+        while not rospy.is_shutdown():
+            self.tick_tock(1)
+
+
+def localization():
+    return pt.composites.Selector(
+        name="Checked localization",
+        children=[isMapClean(), localizeBehaviour()]
+    )
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('main_state_machine')
+    try:
+        BehaviourTree()
+    except rospy.ROSInterruptException:
+        pass
+
+    rospy.spin()
